33ae8b6c58cfc13bb293f4cc1b30a2d0945d00cd
Richard <richardjimenez176@gmail.com>
2020-11-26 14:58:47 -0400

    map cars works!

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -9,66 +9,93 @@ import {
 	IonLabel,
 } from "@ionic/react";
 
-import './Style.css'
-import 'leaflet/dist/leaflet.css';
+import "./Style.css";
 
-import { map, tileLayer, marker } from 'leaflet'
+import "leaflet/dist/leaflet.css";
+import { map, tileLayer, marker, icon } from "leaflet";
 
 interface IProps { }
 
 interface IState {
 	avatar: string;
-	UserName: string
+	UserName: string;
 }
 
 class Home extends React.Component<IProps, IState> {
+
 	constructor(props: any) {
 		super(props);
 
 		this.state = {
 			UserName: "Richard Jimenez",
-			avatar: "https://thispersondoesnotexist.com/image"
-		}
+			avatar: "https://thispersondoesnotexist.com/image",
+		};
 	}
 
-	componentDidUpdate() {
-		var Map = map('map').setView([18.4942592, -69.917081], 15);
+	loadMap() {
 
-		tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(Map);
+		var coords: any;
+
+		navigator.geolocation.getCurrentPosition(({ coords }) => {
+			
+			console.log(coords)
+			var coords = coords;
+
+			console.log(coords, "yola")
+
+			var CarMarker = icon({
+				iconUrl: 'car.svg',
+				iconSize: [38, 95], // size of the icon
+				shadowSize: [50, 64], // size of the shadow
+				iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+				shadowAnchor: [4, 62],  // the same for the shadow
+				popupAnchor: [-3, -76]
+			})
+
+
+			let lat = coords.latitude;
+
+			let long = coords.longitude;
+
+			var Map = map("map").setView([lat, long], 15);
+
+			tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				attribution:
+					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			}).addTo(Map);
+
+			var car = marker([coords.latitude, coords.longitude], { icon: CarMarker }).addTo(Map)
+			var car2 = marker([coords.latitude, coords.longitude], { icon: CarMarker }).addTo(Map)
+
+			setInterval(() => {
+				console.table([lat, long])
+				// lat += 0.0005;
+				long += 0.0005;
+				car2.setLatLng([lat, long])
+				// car.remove()
+			}, 1000)
+		})
 	}
 
 	render() {
-
+		setTimeout(this.loadMap, 500);
 
 		return (
 			<>
 				<IonMenu contentId="main">
-
-
 					<IonItem>
-
 						<IonAvatar>
 							<IonImg src={this.state.avatar} />
 						</IonAvatar>
 
-						<IonLabel>
-							{this.state.UserName}
-						</IonLabel>
-
+						<IonLabel>{this.state.UserName}</IonLabel>
 					</IonItem>
 
 					<IonItemDivider />
-
 				</IonMenu>
 
 				<IonContent id="main">
-					<div id="map">
-						Soy el mapa
-						<button onClick={() => this.setState({ UserName: "yolo" })}>Click me!</button>
-					</div>
-
+					<div id="map"></div>
 				</IonContent>
 			</>
 		);

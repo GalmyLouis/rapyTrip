238a9bdd576a82cb96b602ae68f63c4eeda129be
Richard <richardjimenez176@gmail.com>
2020-11-27 10:25:39 -0400

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { IonApp } from "@ionic/react";
+import { IonApp, IonRouterOutlet, IonRoute } from "@ionic/react";
+import { Redirect } from 'react-router-dom'
+import { IonReactHashRouter } from '@ionic/react-router';
 import Home from "./views/Home";
 
 const App: React.FC = () => {
 	return (
 		<IonApp>
-			<Home />
+
+
+
+			<IonReactHashRouter>
+
+				<IonRouterOutlet />
+			
+				<IonRoute path="/home" exact render={p => {
+					return <Home />
+				}} />
+
+				<Redirect path="/" to="home" exact />
+			
+			</IonReactHashRouter>
+
 		</IonApp>
 	);
 };

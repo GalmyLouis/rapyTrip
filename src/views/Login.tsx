6f22d4cbf4fb91f5ce5df21bf2bc00534ad1db20
Richard <richardjimenez176@gmail.com>
2020-12-01 14:44:50 -0400

    google plus worked

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -23,12 +23,31 @@ async function Login(email: string, pwd: string) {
 
 const SignUp = async (email: string, pwd: string) => {
 	try {
-		await auth.createUserWithEmailAndPassword(email,pwd);
+		await auth.createUserWithEmailAndPassword(email, pwd);
 		location.href = '/';
 	} catch (err) {
 		alert(err);
 	}
 };
+
+const GoogleLogin = async () => {
+	try {
+		const GooglePlusCred = await GooglePlus.login({
+			'scopes': 'profile email ', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+			'webClientId': '852496412611-8829539eehfnhddlf7pq54bt345l0a9k.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+			'offline': true // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+		});
+
+		console.log(GooglePlusCred);
+	}
+	catch (err) {
+		alert("Error at Login")
+		alert(err)
+	}
+}
+
+(window as any).login = GoogleLogin;
+
 
 const LoginForm: React.FC = (props) => {
 	let [email, setEmail] = useState("");
